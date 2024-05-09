3d971f1cb82a763ab63a42808603faa4227c0b25
Minisoni4 <soniamr4@hotmail.com>
2024-05-09 20:06:14 +0200

    Actualización3

--- javaScript.js
+++ javaScript.js
@@ -35,6 +35,6 @@ $form.addEventListener('submit', handleSubmit)
 function handleSubmit(event) {
   event.preventDefault()
   const form = new FormData(this)
-  $buttonMailto.setAttribute('href', `mailto:smonroyr02@educarex.es?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
+  $buttonMailto.setAttribute('href', `mailto:contactoduans@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('message')}`)
   $buttonMailto.click()
 }
